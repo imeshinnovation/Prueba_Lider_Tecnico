dd21f2d066658a7deb47af5e2b68036764736bc2
imeshinnovation <imesh.innovation@gmail.com>
2025-12-24 08:59:03 -0500

    Update

--- ReactJS/user-manager/src/App.tsx
+++ ReactJS/user-manager/src/App.tsx
@@ -50,11 +50,11 @@ function App() {
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                             </svg>
                         </div>
-                        <h1 className="text-3xl font-bold text-gray-800 mb-4">
-                            En este momento el servicio se encuentra en mantenimiento
+                        <h1 className="text-2xl font-bold text-gray-800 mb-4">
+                            En este momento el servicio está en mantenimiento
                         </h1>
-                        <p className="text-lg text-gray-600">
-                            Estamos trabajando para mejorar tu experiencia. Por favor, intenta nuevamente en unos minutos.
+                        <p className="text-small text-gray-600">
+                            Estamos trabajando para mejorar tu experiencia.<br />Por favor, intenta nuevamente en unos minutos.
                         </p>
                     </div>
                 </div>
